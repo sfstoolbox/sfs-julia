9d6a1cd1d4f437840ddbd5107c8b6756511e91dd
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-05-13 14:23:48 +0200

    Renamed the Module

---
+++ src/SoundFieldSynthesis.jl
@@ -0,0 +1,14 @@
+module SoundFieldSynthesis
+
+# import
+
+# is there a better way if we have a great bunch of functions?
+#export mono, imp
+
+# include tools
+include("mathtools.jl")
+include("types.jl")
+# secondary sources
+include("secondarysources.jl")
+
+end # module
