226b0d384a16b8ccf446678f57530779d39fb8d2
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-05-13 20:39:42 +0200

    Made distance of x0 function working

--- src/secondarysources.jl
+++ src/secondarysources.jl
@@ -1,18 +1,19 @@
 # define secondary source type
 # ensure that it is a nx7 matrix
 type SecondarySources
-    positions::Array{Number,2}
-    directions::Array{Number,2}
-    weights::Vector{Number}
+    positions::Array{Float64,2}
+    directions::Array{Float64,2}
+    weights::Vector{Float64}
     # FIXME: make constructor work with new x0 definition
-    SecondarySources(x0::Array) = size(x0,2)!=7 ? error("SecondarySources has to have 7 columns") : new(x0)
+    SecondarySources(x0::Array) = size(x0,2)!=7 ? error("SecondarySources has to have 7 columns") : new(x0[:,1:3],x0[:,4:6],x0[:,7])
+    SecondarySources(positions::Array,directions::Array,weights::Vector) = new(positions,directions,weights)
 end
 
+# add normal functions to work with SecondarySources
 import Base.size
 import Base.length
-# add normal functions to work with SecondarySources
-size(x0::SecondarySources,dim::Integer) = size(x0.x0,1)
-size(x0::SecondarySources) = size(x0.x0,1)
+size(x0::SecondarySources,dim::Integer) = size(x0.positions,1)
+size(x0::SecondarySources) = size(x0.positions,1)
 length(x0::SecondarySources) = size(x0)
 
 # add all the functions dealing with secondary sources
@@ -28,18 +29,16 @@ function distance(x0::SecondarySources,approx::Bool)
             # approximate by using only the first <=100 sources
             x0.positions = x0.positions[1:min(100,size(x0)),:];
         end
-        dx0_single = zeros(size(x0),1);
+        dx0_single = zeros(size(x0),1)
         for ii=1:size(x0)
             # first secondary source position
-            x01 = x0.positions[ii];
+            x01 = x0.positions[ii,:];
             # all other positions
-            x02 = [x0.positions[1:ii-1]; x0.positions[ii+1:end]];
+            x02 = [x0.positions[1:ii-1,:]; x0.positions[ii+1:end,:]];
             # get distance between x01 and all secondary sources within x02
-            dist = vectornorm(broadcast(-,x02,x01),2);
-            # this is maybe the same
-            #dist = vectornorm(x02.-x01,2);
+            dist = vectornorm(x02.-x01,2);
             # get the smallest distance (which is the one to the next source)
-            dx0_single[ii] = min(dist);
+            dx0_single[ii] = minimum(dist);
         end
         # get the mean distance between all secondary sources
         dx0 = mean(dx0_single);
